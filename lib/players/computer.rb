a36cab093a83ebba49510fc5ebd0e9c75e3863a7
Branden Mosley <branden.mosley1337@gmail.com>
2018-03-04 14:53:21 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,24 +2,5 @@ require 'pry'
 module Players
   class Computer < Player
     
-    def initial_move(board)
-      first_moves ||= Game::WIN_COMBINATIONS.sample #pick a random win_combo to play through
-      i = first_moves.sample
-      if board.cells[i] == " "
-        move = i + 1
-        move.to_s
-      else
-        initial_move(board)
-      end
-    end
-    
-    def move(board)
-      sleep 1
-      if board.turn_count > 3
-        initial_move(board)
-      else
-        rand(10).to_s
-      end
-    end    
-  end
+  
 end
