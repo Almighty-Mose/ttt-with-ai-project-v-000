108d221009cbb46afe5a2d20544c980fbaa5e3dd
Branden Mosley <branden.mosley1337@gmail.com>
2018-03-04 15:04:35 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,11 +1,30 @@
-require 'pry'
 module Players
   class Computer < Player
-    
-  def move(board)
-    if !board.taken(5) #we take the center if possible
-      move = "5"
-    elsif board.turn_count == 2
-      move = [1, 3, 7, 9].detect{|n| !board.taken(n)}
-    elsif 
+    def move(board)
+      if !board.taken?(5)
+        move = "5"
+
+      elsif board.turn_count == 1
+        move = "1"
+
+      elsif board.turn_count == 2
+        move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
+
+      else
+        Game::WIN_COMBINATIONS.detect do |cmb|
+
+          if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+
+          elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+          end
+        end
+
+        # If none of the WIN_COMBINATIONS patterns have two squares taken by the same token and a third empty square, play into the first open square you find, first checking corners and then checking sides.
+        move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
+      end
+      move
+    end
+  end
 end
