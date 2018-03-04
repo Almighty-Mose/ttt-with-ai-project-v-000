98816741e719754d93041176aa72a127c36aa9d5
Branden Mosley <branden.mosley1337@gmail.com>
2018-03-04 15:01:15 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,5 +3,9 @@ module Players
   class Computer < Player
     
   def move(board)
-    move = "5" if !board.taken(5)
+    if !board.taken(5) #we take the center if possible
+      move = "5"
+    elsif board.taken(5)
+      move = [1, 3, 7, 9].detect{|n| !board.taken(n)}
+    elsif 
 end
